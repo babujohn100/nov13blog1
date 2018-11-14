48ac3178815fc09ae3b213fbe108562eb913c21f
BABU JOHN <bbuj27@gmail.com>
2018-11-14 12:44:32 +0000

    hello

--- django_blog/settings/base.py
+++ django_blog/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oct13blog1-bbuj27.c9users.io']
+ALLOWED_HOSTS = ['oct13blog1-bbuj27.c9users.io', 'nov13blog1.herokuapp.com' ]
 
 
 # Application definition
